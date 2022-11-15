5ac973acabf86555b3a91d6743cd1334a9949196
Tobi <tobiradeniji@gmail.com>
2022-11-15 20:56:50 +0000

    load more results when scroll to the bottom

--- src/App.js
+++ src/App.js
@@ -3,43 +3,63 @@ import { View } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useState } from "react";
 import { styles } from "./AppStyles";
-import favicon from "../assets/favicon.png"
+import favicon from "../assets/favicon.png";
 import Form from "./components/Form/Form";
 import RecipeList from "./components/RecipeList/RecipeList";
-
-
 
 export default function App() {
 	const [hasResults, setHasResults] = useState(false);
 	const [recipes, setRecipes] = useState([
 		{
-			"name": "No Recipe Name Available",
-			"image": favicon,
-			"ingredients": ["None"],
-			"source": ""
-		}]);
+			name: "No Recipe Name Available",
+			image: favicon,
+			ingredients: ["None"],
+			source: "",
+		},
+	]);
 	const [addRecipesLink, setAddRecipesLink] = useState("");
 
-	function addRecipes(recipes)
-	{	
+	function GetRecipes(recipeList) {
 		let newState = [];
-		for (let i = 0; i < recipes.length; i++)
-		{
-			newState.push(recipes[i]);
+		for (let i = 0; i < recipeList.length; i++) {
+			newState.push(recipeList[i]);
 		}
 
 		setRecipes(newState);
 		setHasResults(true);
 	}
 
+	function addRecipes(recipeList)
+	{
+		let newState = [];
+
+		for (let i = 0; i < recipeList.length; i++) {
+			newState.push(recipeList[i]);
+			
+		}
+
+		for (let i = 0; i < recipes.length; i++) {
+			newState.push(recipes[i]);
+		}
+
+		setRecipes(newState);
+	}
+
 	return (
 		<View style={styles.container}>
-			<RecipeList hasResults={hasResults} recipes={recipes} addRecipesLink={addRecipesLink} />
+			<RecipeList
+				setData={addRecipes}
+				setLink={setAddRecipesLink}
+				hasResults={hasResults}
+				recipes={recipes}
+				addRecipesLink={addRecipesLink}
+			/>
 
-			<Form setData={addRecipes} setLink={setAddRecipesLink}/>
+			<Form
+				setData={GetRecipes}
+				setLink={setAddRecipesLink}
+			/>
 			<StatusBar style="auto" />
 		</View>
 	);
-
-	
 }
--- src/components/RecipeList/RecipeList.js
+++ src/components/RecipeList/RecipeList.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { FlatList, Image, Text, View } from "react-native";
+import BackEndIP from "../../ipaddressesports/BackEndIP";
 import IngredientList from "../IngredientList/IngredientList";
 import { recipeListStyle } from "./RecipeListStyle";
 
@@ -26,11 +27,13 @@ export default function RecipeList(props) {
             let response = await axios({
                 method: "get",
                 url:
-                    "http://" + BackEndIP + "/additionalRecipes/?ingredients=" + props.additionalRecipes,
+                    "http://" + BackEndIP + "/addRecipes/?nextLink=" + props.addRecipesLink,
                 responseType: "json",
             });
 
             let content = await response.data;
+			props.setData(content.results);
+			props.setLink(content.addRecipesLink);
         }
     }
 
